7813f5cbf59d6972aa0c13a7905b65bca80dc476
aminzadeh.majid@gmail.com <aminzadeh.majid@gmail.com>
2022-01-30 21:45:45 +0330

    init commit

---
+++ src/main/java/com/tosan/calculator/Adder.java
@@ -0,0 +1,7 @@
+package com.tosan.calculator;
+
+public class Adder {
+    public int add(int var1, int var2){
+        return var1 + var2 + 3;
+    }
+}
---
+++ src/test/java/com/tosan/exmaple1/Test.java
@@ -0,0 +1,9 @@
+package com.tosan.exmaple1;
+
+
+public class Test {
+    @org.junit.gen5.api.Test
+    public void test(){
+
+    }
+}
